abdbe1e5a288ba62ee4acbbad37f99a7094d1391
SyafiqSafiyan <syafiqsafiyan@gmail.com>
2021-12-14 11:02:29 +0800

    Update status.php

--- status.php
+++ status.php
@@ -36,15 +36,27 @@ if(isset($_GET['api_key_value']) == 'abc123')
 	echo '<br>';
 	echo '<br>';
 
+
+	if ($myObj->px_status == 0)
+	{
+		$NewStatus = 1;
+	}
+	else
+	{
+		$NewStatus = 0;
+	}
+
 	$posts = array('px_set_point'=> $myObj->px_set_point, 
 				   'px_set_back'=> $myObj->px_set_back, 
 				   'px_high_temp'=> $myObj->px_high_temp, 
-				   'px_status'=> $myObj->px_status, 
+				   'px_status'=> $NewStatus, 
 				   'api_key_value'=> $myObj->api_key_value);
 
 	$fp = fopen('php.json', 'w');
 	fwrite($fp, json_encode($posts,JSON_NUMERIC_CHECK));
 	fclose($fp);
+
+
 
 	$curentjson = file_get_contents("php.json");
 	$myObjcrr = json_decode($curentjson , false);
